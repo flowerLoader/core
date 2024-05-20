8328a7717c04239742c2fe8829e1f51b56166e01
Robin <none>
2024-05-20 16:57:51 -0500

    convert flowerful.patches to object

--- flowerful.patches.ts
+++ flowerful.patches.ts
@@ -1,142 +1,153 @@
+import { LogSource } from "@flowerloader/api";
 import { FlowerPatch, Patchable, PatchFn } from "@flowerloader/api/FlowerPatch";
-import { WriteLog, WriteDebug } from "./flowerful";
 
-const patches: FlowerPatch[] = [];
-
-//Apply patches
-export function ApplyAllPatches()
+export class flowerPatcher
 {
-    for (const patch of patches)
-    {
-        Apply(patch)
-    }
-}
+    Patches: FlowerPatch[] = [];
+    MyLogSource: LogSource;
 
-function FindPatch(obj: Patchable, method: string)
-{
-    if (!obj[method])
-    {
-        console.error(`Method ${method} not found on ${obj}`);
-        return;
-    }
 
-    for (const patch of patches)
+    //Apply patches
+    ApplyAllPatches()
     {
-        if (patch.obj === obj && patch.methodName === method)
+        for (const patch of this.Patches)
         {
-            return patch;
+            this.Apply(patch)
         }
     }
 
-    const patch: FlowerPatch = {
-        obj: obj,
-        methodName: method,
-        prefixes: [],
-        postfixes: [],
-        applied: false,
-    }
-
-    patches.push(patch);
-    return patch;
-}
-
-/**
- * Binds a patch to an object. All patches are accumulated even after initial binding.
- * @param patch 
- * @returns false if this patch has already been bound
- */
-function Apply(patch: FlowerPatch): boolean
-{
-
-    /** Only apply patches once ever */
-    if (patch.applied)
-        return false;
-
-    /* eslint-disable-next-line @typescript-eslint/ban-types */
-    const orig = patch.obj[patch.methodName] as Function;
-
-    const wrapper: PatchFn = function (...args)
+    FindPatch(obj: Patchable, method: string)
     {
-        WriteDebug(`Running detour for ${patch.methodName}`);
-        // <-- this = obj
-
-        patch = FindPatch(patch.obj, patch.methodName)!;
-
-        WriteDebug(`Prefixes ${patch.prefixes.length}`);
-        //patch.prefixes.forEach(prefix => prefix.call(patch.obj, ...args));
-        //Allow ending the detour early
-        for (const prefix of patch.prefixes)
+        if (!obj[method])
         {
-            if (false === prefix.call(patch.obj, ...args))
-            {
-                WriteDebug("Ending detour");
-                return;
-            }
-        }
-
-        let origRet;
-
-        try
-        {
-            origRet = orig.call(patch.obj, ...args);
-        }
-        catch (e)
-        {
-            WriteLog("Flower", `Error running orig: ${e}`)
+            console.error(`Method ${method} not found on ${obj}`);
             return;
         }
 
-        /**
-         * Todo: allow postfixes to modify the return data here
-         */
-
-        WriteDebug(`Postfixes ${patch.postfixes.length}`);
-
-        for (const postfix of patch.postfixes)
+        for (const patch of this.Patches)
         {
-            try
+            if (patch.obj === obj && patch.methodName === method)
             {
-                postfix.call(patch.obj, ...args)
-            }
-            catch (e: any)
-            {
-                WriteLog("Flower", `Failed to run postfix: ${e.message}`);
+                return patch;
             }
         }
 
+        const patch: FlowerPatch = {
+            obj: obj,
+            methodName: method,
+            prefixes: [],
+            postfixes: [],
+            applied: false,
+        }
 
-        return origRet;
+        this.Patches.push(patch);
+        return patch;
     }
 
-    patch.obj[patch.methodName] = wrapper.bind(patch.obj);
-    return true;
-}
-
-/**
- * Registers a new patch with flower
- * @param obj any object that contains a method
- * @param methodName the string name of the method to patch
- * @param patch a function that runs when the method is called
- * @param isPrefix if this should run before the method (afterward otherwise)
- * @returns true on success
- */
-export function RegisterPatch(obj: Patchable, methodName: string, patch: PatchFn, isPrefix: boolean)
-{
-
-    WriteDebug(`Running RegisterPatch for ${methodName}`);
-
-    const accum = FindPatch(obj, methodName);
-    if (!accum) return false;
-
-    if (isPrefix)
+    /**
+     * Binds a patch to an object. All patches are accumulated even after initial binding.
+     * @param patch 
+     * @returns false if this patch has already been bound
+     */
+    Apply(patch: FlowerPatch): boolean
     {
-        accum.prefixes.push(patch);
-    }
-    else
-    {
-        accum.postfixes.push(patch);
+
+        /** Only apply patches once ever */
+        if (patch.applied)
+            return false;
+
+        /* eslint-disable-next-line @typescript-eslint/ban-types */
+        const orig = patch.obj[patch.methodName] as Function;
+        const Patcher = this;
+
+        const wrapper: PatchFn = function (...args)
+        {
+            Patcher.MyLogSource.writeDebug(`Running detour for ${patch.methodName}`);
+            // <-- this = obj
+
+            patch = Patcher.FindPatch(patch.obj, patch.methodName)!;
+
+            Patcher.MyLogSource.writeDebug(`Prefixes ${patch.prefixes.length}`);
+            //patch.prefixes.forEach(prefix => prefix.call(patch.obj, ...args));
+            //Allow ending the detour early
+            for (const prefix of patch.prefixes)
+            {
+                if (false === prefix.call(patch.obj, ...args))
+                {
+                    Patcher.MyLogSource.writeDebug("Ending detour");
+                    return;
+                }
+            }
+
+            let origRet;
+
+            try
+            {
+                origRet = orig.call(patch.obj, ...args);
+            }
+            catch (e)
+            {
+                Patcher.MyLogSource.write(`Error running orig: ${e}`)
+                return;
+            }
+
+            /**
+             * Todo: allow postfixes to modify the return data here
+             */
+
+            Patcher.MyLogSource.writeDebug(`Postfixes ${patch.postfixes.length}`);
+
+            for (const postfix of patch.postfixes)
+            {
+                try
+                {
+                    postfix.call(patch.obj, ...args)
+                }
+                catch (e: any)
+                {
+                    Patcher.MyLogSource.write(`Failed to run postfix: ${e.message}`);
+                }
+            }
+
+
+            return origRet;
+        }
+
+        patch.obj[patch.methodName] = wrapper.bind(patch.obj);
+        return true;
     }
 
-    Apply(accum);
-    return true;
+    /**
+     * Registers a new patch with flower
+     * @param obj any object that contains a method
+     * @param methodName the string name of the method to patch
+     * @param patch a function that runs when the method is called
+     * @param isPrefix if this should run before the method (afterward otherwise)
+     * @returns true on success
+     */
+    RegisterPatch(obj: Patchable, methodName: string, patch: PatchFn, isPrefix: boolean)
+    {
+
+        this.MyLogSource.writeDebug(`Running RegisterPatch for ${methodName}`);
+
+        const accum = this.FindPatch(obj, methodName);
+        if (!accum) return false;
+
+        if (isPrefix)
+        {
+            accum.prefixes.push(patch);
+        }
+        else
+        {
+            accum.postfixes.push(patch);
+        }
+
+        this.Apply(accum);
+        return true;
+    }
+
+    constructor(LogSource: LogSource)
+    {
+        this.MyLogSource = LogSource;
+    }
 }
