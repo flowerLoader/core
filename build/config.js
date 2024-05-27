1ff6d395bdcdb4b30b77b0edcc7861e9166a81c7
Robin <none>
2024-05-27 13:38:19 -0500

    fix default config, hehe

--- build/config.js
+++ build/config.js
@@ -1,7 +1,7 @@
-ENABLED_PLUGINS = [
+export const ENABLED_PLUGINS = [
 	/*
 	 * each entry in this list should be a quoted string with the full name
 	 * of the file relative to the plugins folder and end in a comma
-	 * "flowerteam.plugintemplate.js" <= like this
+	 * "flowerteam.plugintemplate.js", <= like this
 	 */
 ];
